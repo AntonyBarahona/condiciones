80c7f2d5dd468972c653150b8d8d838d52437918
AntonyBarahona <barahouf25@gmail.com>
2023-02-18 13:12:29 -0600

    Trabajo en clase

---
+++ condiciones.py
@@ -0,0 +1,8 @@
+ # Un conjunto de personas se aserca a una fiesta de cumpleaños para recibir su refacciòn, la rafaccion de determina por medio de la edad: menores a 18 años se le entregara pastel y a mayores de edad seran tacos
+edad=int(input("Ingrese su edad "))
+if edad<8:
+    print("Disfrute sus dulces ")
+elif edad<18:
+    print("Disfrute su pastel ")
+else:
+    print("desfrute su taco")
